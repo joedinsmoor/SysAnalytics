68e56b8a0e50019e67e84a34746b2b6f7c5e96be
Joe Dinsmoor <jdinsmoor@dinsmoor.pw>
2024-03-12 22:02:50 -0400

    Updates with network usage

--- sys_report.py
+++ sys_report.py
@@ -11,22 +11,34 @@ def system_analytics_pretty():
     cpu_usage = psutil.cpu_percent(interval=1)
     memory_usage = psutil.virtual_memory().percent
     disk_usage = psutil.disk_usage('/').percent
+    network_info = psutil.net_io_counters()
+    network_status = {
+        'sent_bytes': network_info.bytes_sent,
+        'received_bytes': network_info.bytes_recv
+    }
     
     return render_template(template_name_or_list='sys_analytics.html', 
                            cpu_usage=cpu_usage,
                            memory_usage=memory_usage,
-                           disk_usage=disk_usage)
+                           disk_usage=disk_usage,
+                           network_status=network_status)
 
 @app.route('/system-analytics-raw')
 def system_analytics():
     cpu_usage = psutil.cpu_percent(interval=1)
     memory_usage = psutil.virtual_memory().percent
     disk_usage = psutil.disk_usage('/').percent
+    network_info = psutil.net_io_counters()
+    network_status = {
+        'sent_bytes': network_info.bytes_sent,
+        'received_bytes': network_info.bytes_recv
+    }
     
     analytics = {
         "cpu_usage": cpu_usage,
         "memory_usage": memory_usage,
-        "disk_usage": disk_usage
+        "disk_usage": disk_usage,
+        "network_status": network_status
     }
     
     return jsonify(analytics)
